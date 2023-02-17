8cd067b7b3d4a2aae7b525b99a4cbd6447e5fb26
Tuhin Ahmed <117445766+JessicamMnQShar@users.noreply.github.com>
2023-02-17 08:41:04 +0600

    Add files via upload

---
+++ pkg.sh
@@ -0,0 +1,69 @@
+clear
+echo "
+
+
+████████ ██    ██ ██   ██ ██ ███    ██ 
+   ██    ██    ██ ██   ██ ██ ████   ██ 
+   ██    ██    ██ ███████ ██ ██ ██  ██ 
+   ██    ██    ██ ██   ██ ██ ██  ██ ██ 
+   ██     ██████  ██   ██ ██ ██   ████ 
+                                       
+                                       
+" lolcat
+echo "======================================="
+echo " ('\033[1;91m[\033[1;92m1\033[1;91m]\033[1;97m   Tools Name : BASIC PACKAGE INSTALL"
+echo " ('\033[1;91m[\033[1;92m1\033[1;91m]\033[1;97m   Author : TUHIN AHMED"
+echo " ('\033[1;91m[\033[1;92m1\033[1;91m]\033[1;97m   FB ACCOUNT: MOHAMMAD TUHIN AHMED"
+echo "======================================="
+termux-setup-storage
+
+apt update
+
+apt upgrade -y
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install python3 -y
+
+pkg install git -y
+
+pkg install bash -y
+
+pkg install figlet -y
+
+pkg install toilet -y
+
+pkg install nano -y
+
+pkg install php -y
+
+pkg install pip -y
+
+pkg install pip2 -y
+
+pip2 install requests as -y
+
+pip install future -y
+
+pip2 install requirements -y
+
+apt install ruby -y
+
+apt install openssh -y 
+
+apt install wget -y
+
+apt install curl -y
+
+apt install proof -y
+
+pkg install proot-distro
+
+Pkg install fish 
+
+
+echo ""
+echo ""
+echo "All Package Installed"
